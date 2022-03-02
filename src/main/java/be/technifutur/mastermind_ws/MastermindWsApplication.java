93e501fa075696285008699991b934a801a2257d
David Ratz <david.ratz@hotmail.com>
2022-03-02 16:12:08 +0100

    Mastermind web services init

---
+++ src/main/java/be/technifutur/mastermind_ws/MastermindWsApplication.java
@@ -0,0 +1,13 @@
+package be.technifutur.mastermind_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastermindWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastermindWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/technifutur/mastermind_ws/MastermindWsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technifutur.mastermind_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MastermindWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
